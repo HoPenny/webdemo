e339bf649eb96f39d266d478adccbc07687ac985
Penny Ho <hopenny00@gmail.com>
2022-12-19 16:30:12 +0800

    init1219

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webdemo.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult Login()
+        {
+            vmLogin model = new vmLogin();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Login(vmLogin model)
+        {
+            //驗證不合格返回View
+            if (!ModelState.IsValid) return View(model);
+            //記錄登入者資訊
+            Session["UserNo"] = model.UserNo;
+            Session["UserName"] = "Penny";
+            Session["IsLogin"] = true;
+            //回到首頁
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ Models/ViewModel/vmLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+///<summary>
+///使用者登入用ViewModel
+///</summary>
+public class vmLogin
+    {
+    [Display(Name ="登入帳號")]
+    [Required(ErrorMessage = "登入帳號不可空白")]
+    public string UserNo { get; set; }
+
+    [Display(Name = "登入密碼")]
+    [Required(ErrorMessage = "登入密碼不可空白")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+
+}
